3c4b4f904ae655d90eb1157b219eff3c4bcd86c1
eijiensing <eijitron@gmail.com>
2026-07-07 16:30:14 +0200

    yeah

---
+++ src/importer.rs
@@ -0,0 +1,656 @@
+use serde_json::Value;
+use std::fs::File;
+
+// --- Your Target Domain Structs ---
+
+pub enum Tag {
+    Abyss4AdditionalChance,
+    AbyssAdditionalChance,
+    AbyssDepthsChance,
+    AbyssEnhancedMonstersPerChasm,
+    AbyssExtraModifiers,
+    AbyssExtraTickets,
+    AbyssIncreasedRewards,
+    AbyssJewelMod,
+    AbyssMonsterIncrease,
+    AbyssRareMonsterIncrease,
+    AdditionalAmmo,
+    AdditionalArrows,
+    AdditionalBallistaTotem,
+    AdditionalBlock,
+    AdditionalCharm,
+    AdditionalComboChance,
+    AdditionalProjectiles,
+    AdditionalSpellTotem,
+    AdditionalTotems,
+    AftershockChance,
+    AilmentChance,
+    AilmentEffect,
+    AilmentThreshold,
+    AilmentThresholdfromEnergyShield,
+    AllAttributes,
+    AllDamage,
+    AllDefences,
+    AllResistances,
+    ArcaneSurgeEffect,
+    ArcaneSurgeOnCrit,
+    ArchonDelayRecovery,
+    ArchonDuration,
+    AreaOfEffect,
+    ArmourAppliesToChaosDamage,
+    ArmourAppliesToElementalDamage,
+    ArmourBreak,
+    ArmourBreakDuration,
+    ArmourPenetration,
+    AttackDamage,
+    AuraEffect,
+    BannerArea,
+    BannerDuration,
+    BannerValourGained,
+    BaseCurseDuration,
+    BaseLocalDefences,
+    BaseLocalDefencesAndDefencePercent,
+    BaseLocalDefencesAndLife,
+    BaseLocalDefencesAndMana,
+    BaseRunicWard,
+    BaseSpirit,
+    BellHitLimit,
+    BeltCharmChargesGained,
+    BeltCharmChargesUsed,
+    BeltCharmDuration,
+    BeltFlaskChargesGained,
+    BeltFlaskChargesUsed,
+    BeltFlaskRecoveryRate,
+    BleedChanceIncrease,
+    BleedDuration,
+    BleedingDamage,
+    BlindEffect,
+    BlindOnHit,
+    BlindingHit,
+    BlockDamageTakenWhileActiveBlocking,
+    BodyArmourFromBodyArmour,
+    Breach3AdditionalChance,
+    BreachAdditionalChance,
+    BreachAdditionalRares,
+    BreachBossChance,
+    BreachChestAdditional,
+    BreachHivebloodQuantity,
+    BreachMagicMonsterIncrease,
+    BreachMonsterQuantity,
+    BreachMonsterSplinterIncrease,
+    BreachRareMonsterIncrease,
+    BreachRareMonsterPotency,
+    BreachSpeedIncrease,
+    BreachWombgiftQuantity,
+    CastSpeedOnFullLife,
+    CastSpeedOnLowLife,
+    Chain,
+    ChainFromTerrain,
+    ChanceToGainAnAdditionalInfusion,
+    ChanceToGainNaturesArchon,
+    ChanceToPierce,
+    ChanceToTakeCriticalStrike,
+    ChaosResistance,
+    CharmChargesGained,
+    CharmChargesUsed,
+    CharmDamageWhileUsing,
+    CharmDuration,
+    ColdAndChaosDamageResistance,
+    ColdDamage,
+    ColdDamagePercentage,
+    ColdDamagePercentagePrefix,
+    ColdDamageTakenRecoupedAsLife,
+    ColdResistance,
+    ColdResistancePenetration,
+    CompanionAttackSpeed,
+    CompanionDamage,
+    CompanionLife,
+    CompanionReservationEfficiency,
+    CooldownRecovery,
+    CorruptedBlood,
+    CorruptionIntertactions,
+    CriticalAilmentEffect,
+    CriticalStrikeChanceIncrease,
+    CriticalStrikeMultiplier,
+    CrossbowDamage,
+    CrossbowReloadSpeed,
+    CrossbowSpeed,
+    CurseAreaOfEffect,
+    CurseCastSpeed,
+    CurseDelay,
+    CurseEffectiveness,
+    DamageForm,
+    DamageRemovedFromManaBeforeLife,
+    DamageTakenGainedAsLife,
+    DamageVsRareOrUnique,
+    DamageWithTriggeredSpells,
+    DamageWithWeaponTypeSkill,
+    DamagevsArmourBrokenEnemies,
+    DamagingAilmentDuration,
+    DazeBuildup,
+    DebuffTime,
+    DefencesPercent,
+    DefencesPercentAndStunThreshold,
+    DeflectDamageTaken,
+    DeliriumAdditionalRewardType,
+    DeliriumBossChance,
+    DeliriumDifficultyIncrease,
+    DeliriumDoodadsIncrease,
+    DeliriumFogDissipationDelay,
+    DeliriumFogPersistence,
+    DeliriumMonsterSplinterIncrease,
+    DeliriumPackSizeIncrease,
+    DeliriumRareMonsterPause,
+    DeliriumRewardProgressIncrease,
+    Dexterity,
+    DodgeRoll,
+    ElementalAilmentDuration,
+    ElementalAilmentEffect,
+    ElementalDamagePercent,
+    ElementalDamageTakenRecoupedAsEnergyShield,
+    ElementalInfusion,
+    ElementalPenetration,
+    ElementalSkillLimit,
+    EnchantmentHeistArmour,
+    Energy,
+    EnergyShieldDelay,
+    EnergyShieldPercent,
+    EnergyShieldRechargeRateIfBlockedRecently,
+    EnergyShieldRegeneration,
+    EquipmentModifierEffect,
+    EssenceAbyss,
+    EssenceGoldDropped,
+    EssenceGrantedPassive,
+    EvasionAppliesToDeflection,
+    EvasionRatingFromBodyArmour,
+    EvasionRatingPercent,
+    ExertedAttackDamage,
+    ExpeditionArtifactIncrease,
+    ExpeditionExplosionPlacement,
+    ExpeditionExplosionRadius,
+    ExpeditionLogbookIncrease,
+    ExpeditionRareMonsters,
+    ExpeditionRelicIncrease,
+    ExpeditionRelicModEffect,
+    ExpeditionRunicMonsters,
+    ExposureEffect,
+    FasterAilmentDamage,
+    FireAndChaosDamageResistance,
+    FireDamage,
+    FireDamagePercentage,
+    FireDamagePercentagePrefix,
+    FireDamageTakenRecoupedAsLife,
+    FireResistance,
+    FireResistancePenetration,
+    FissureChance,
+    FlaskBuffWhileHealing,
+    FlaskChargeGeneration,
+    FlaskChargeGenerationPercent,
+    FlaskChargesUsed,
+    FlaskDuration,
+    FlaskExtraLifeRegeneration,
+    FlaskGainCharge,
+    FlaskHealsOthers,
+    FlaskLifeRecovery,
+    FlaskManaRecovery,
+    FlaskNumCharges,
+    FlaskRechargeRate,
+    FlaskRecoveryAmount,
+    FlaskRecoverySpeed,
+    FocusEnergyShield,
+    ForkingProjectiles,
+    FormCritMultiplier,
+    FreezeDamageIncrease,
+    FreezeThreshold,
+    GainFlatLifeOnBlock,
+    GainLifeOnBlock,
+    GainManaOnBlock,
+    GainRage,
+    GlobalDeflectionRating,
+    GlobalItemAttributeRequirements,
+    GlobalSkillGemQuality,
+    GloryChanceToNotConsume,
+    GloryGeneration,
+    GrenadeAdditionalDetonationChance,
+    GrenadeCooldownUse,
+    GrenadeDamage,
+    GrenadeDuration,
+    HazardDamage,
+    HeavyStunDecayRate,
+    HeraldDamage,
+    HeraldReservationEfficiency,
+    HinderedEnemyTakeIncreasedDamage,
+    IgniteChanceIncrease,
+    IgniteEffect,
+    Immobilise,
+    ImmuneToMaim,
+    ImmunityToBlind,
+    IncisionChance,
+    IncreaseSocketedGemLevel,
+    IncreasedAccuracy,
+    IncreasedAccuracyPercent,
+    IncreasedAttackAreaOfEffect,
+    IncreasedAttackSpeed,
+    IncreasedBlockChance,
+    IncreasedCastSpeed,
+    IncreasedChaosDamage,
+    IncreasedChillDuration,
+    IncreasedEnergyShield,
+    IncreasedEvasionRating,
+    IncreasedLife,
+    IncreasedMana,
+    IncreasedMaximumPowerCharges,
+    IncreasedMinionDamageIfYouHitEnemy,
+    IncreasedPhysicalDamageReductionRating,
+    IncreasedPhysicalDamageReductionRatingPercent,
+    IncreasedShieldBlockPercentage,
+    IncreasedSpeed,
+    IncreasedSpellDamageOnFullEnergyShield,
+    IncreasedStunThreshold,
+    IncreasedStunThresholdIfNoRecentStun,
+    IncreasedTotemLife,
+    IncreasedWeaponElementalDamagePercent,
+    IncursionSecondaryEncounters,
+    InstantLeechPercent,
+    Intelligence,
+    InvocatedSpellHalfEnergyChance,
+    ItemFoundRarityIncrease,
+    ItemFoundRarityIncreasePrefix,
+    Knockback,
+    LifeCost,
+    LifeCostEfficiency,
+    LifeGainPerTarget,
+    LifeGainedFromEnemyDeath,
+    LifeLeech,
+    LifeRegeneration,
+    LifeRegenerationPlusPercentWhileMoving,
+    LifeRegenerationRate,
+    LightRadiusAndAccuracy,
+    LightRadiusAndManaRegeneration,
+    LightningAndChaosDamageResistance,
+    LightningDamage,
+    LightningDamageCanIgnite,
+    LightningDamagePercentage,
+    LightningDamagePercentagePrefix,
+    LightningDamageTakenRecoupedAsLife,
+    LightningResistance,
+    LightningResistancePenetration,
+    LocalAttributeRequirements,
+    LocalChanceToBleed,
+    LocalChanceToPoisonOnHit,
+    LocalChaosDamage,
+    LocalIncreasedPhysicalDamagePercentAndAccuracyRating,
+    LocalIncreasedSpiritAndMana,
+    LocalIncreasedSpiritPercent,
+    LocalMaimOnHit,
+    LocalMaximumQuality,
+    LocalPhysicalDamagePercent,
+    LocalRunicWardPercent,
+    MaceStun,
+    ManaCostEfficiency,
+    ManaGainedFromEnemyDeath,
+    ManaGainedOnKillPercentage,
+    ManaLeech,
+    ManaLeechAmount,
+    ManaRegeneration,
+    MapAdditionalAzmeriWisp,
+    MapAdditionalChests,
+    MapAdditionalEssence,
+    MapAdditionalExile,
+    MapAdditionalModifier,
+    MapAdditionalShrine,
+    MapAdditionalSpirit,
+    MapAdditionalStoneCircle,
+    MapAdditionalStrongbox,
+    MapAdditionalUniqueMonsterModifier,
+    MapBleeding,
+    MapBossAdditionalEssence,
+    MapBossAdditionalShrine,
+    MapBossAdditionalSpirit,
+    MapBossAdditionalStrongbox,
+    MapBossExperience,
+    MapBossQuantity,
+    MapBossRarity,
+    MapBossWaystoneChance,
+    MapChestCountIncrease,
+    MapDroppedGoldIncrease,
+    MapDroppedItemQuantityIncrease,
+    MapDroppedItemQuantityRarityIncrease,
+    MapDroppedItemRarityIncrease,
+    MapDroppedMapsIncrease,
+    MapExperienceGainIncrease,
+    MapMagicChestCountIncrease,
+    MapMagicPackIncrease,
+    MapMagicPackSizeIncrease,
+    MapMonsterAdditionalPacks,
+    MapMonsterAreaOfEffect,
+    MapMonsterChaosDamage,
+    MapMonsterColdDamage,
+    MapMonsterCriticalStrikesAndDamage,
+    MapMonsterDamage,
+    MapMonsterEffectiveness,
+    MapMonsterFast,
+    MapMonsterFireDamage,
+    MapMonsterLife,
+    MapMonsterLightningDamage,
+    MapMonsterMultipleProjectiles,
+    MapMonsterRarityIncrease,
+    MapMonstersAccuracy,
+    MapMonstersAilmentChance,
+    MapMonstersAllResistances,
+    MapMonstersArmourBreak,
+    MapMonstersArmoured,
+    MapMonstersBaseSelfCriticalMultiplier,
+    MapMonstersCurseEffectOnSelfFinal,
+    MapMonstersElementalPenetration,
+    MapMonstersEnergyShield,
+    MapMonstersEvasive,
+    MapMonstersStealChargesOnHit,
+    MapMonstersStunAndAilmentThreshold,
+    MapMonstersStunBuildup,
+    MapPackSizeIncrease,
+    MapPlayerCooldownRecovery,
+    MapPlayerElementalWeakness,
+    MapPlayerEnfeeblement,
+    MapPlayerMaxResists,
+    MapPlayerReducedRegen,
+    MapPlayerTemporalChains,
+    MapPlayersGainReducedFlaskCharges,
+    MapPoisoning,
+    MapRareChestCountIncrease,
+    MapRareMonstersAdditionalModifier,
+    MapRarePackIncrease,
+    MapSpreadGroundEffect,
+    MapTotalEffectivenessIncrease,
+    MarkCritical,
+    MarkDamage,
+    MarkDuration,
+    MartialWeaponGainedDamage,
+    MaximumBlockChance,
+    MaximumChaosResistance,
+    MaximumColdResist,
+    MaximumEnduranceCharges,
+    MaximumEnergyShieldFromBodyArmour,
+    MaximumFireResist,
+    MaximumFrenzyCharges,
+    MaximumLifeIncreasePercent,
+    MaximumLifeOnKillPercent,
+    MaximumLightningResistance,
+    MaximumManaIncreasePercent,
+    MaximumPuppeteerStacks,
+    MaximumRage,
+    MaximumResistances,
+    MeleeDamage,
+    MeleeDamageIfProjectileHitRecently,
+    MeleeWeaponAndUnarmedRange,
+    MinionAccuracyRating,
+    MinionAreaOfEffect,
+    MinionAttackSpeedAndCastSpeed,
+    MinionChaosResistance,
+    MinionCooldown,
+    MinionCriticalStrikeChance,
+    MinionCriticalStrikeMultiplier,
+    MinionDamage,
+    MinionDamagingAilments,
+    MinionElementalResistances,
+    MinionGainPuppetMasterOnCommand,
+    MinionLife,
+    MinionPhysicalDamageReduction,
+    MovementVelocity,
+    OfferingDuration,
+    OfferingEffect,
+    OfferingLife,
+    Onslaught,
+    ParriedDebuff,
+    PercentDamageGoesToMana,
+    PercentageDexterity,
+    PercentageIntelligence,
+    PercentageStrength,
+    PhysicalDamage,
+    PhysicalDamagePercent,
+    PhysicalDamageTakenAsChaos,
+    PhysicalDamageTakenAsLightningWhileActiveBlocking,
+    PinBuildup,
+    PoisonChanceIncrease,
+    PoisonDuration,
+    PoisonEffect,
+    PresenceRadius,
+    ProjectileDamage,
+    ProjectileDamageIfMeleeHitRecently,
+    ProjectileSpeed,
+    QuarterstaffFreezeBuildup,
+    RageOnHit,
+    RecoverPercentMaxLifeOnKill,
+    RecoverResourceOnExpendingCombo,
+    RecoverRunicWardOnCharmUse,
+    ReducedAilmentDuration,
+    ReducedCriticalStrikeDamageTaken,
+    ReducedCurseEffect,
+    ReducedExtraDamageFromCrits,
+    ReducedPhysicalDamageTaken,
+    ReducedSelfIgniteDuration,
+    RemnantEffect,
+    RemnantPickupRadius,
+    RitualAdditionalReroll,
+    RitualChanceForNoCost,
+    RitualDeferCostIncrease,
+    RitualDeferSpeed,
+    RitualMagicMonsters,
+    RitualOmenChance,
+    RitualRareMonsters,
+    RitualRerollCostIncrease,
+    RitualTributeIncrease,
+    RunicWardPercent,
+    ShieldArmourIncrease,
+    ShieldSkillsStunningAlsoFullyBreaksArmour,
+    ShockChanceIncrease,
+    ShockDuration,
+    ShockEffect,
+    SkillCostEfficiency,
+    SkillEffectDuration,
+    SkillLifeCost,
+    SlowEffect,
+    SlowPotency,
+    SoulCore,
+    SpearAttackSpeed,
+    SpearCriticalDamage,
+    SpearDamage,
+    SpecificWeaponAccuracy,
+    SpecificWeaponDamage,
+    SpecificWeaponSpeed,
+    SpellAreaOfEffect,
+    SpellCriticalStrikeChanceIncrease,
+    SpellDamage,
+    SpellDamageAndMana,
+    SpellsFire2AdditionalProjectileChance,
+    SpiritReservationEfficiency,
+    Strength,
+    StunDamageIncrease,
+    StunDurationIncreasePercent,
+    StunThreshold,
+    StunThresholdDuringParry,
+    StunThresholdfromEnergyShield,
+    SummonTotemCastSpeed,
+    TemporaryMinionLimit,
+    Thorns,
+    TotemDamage,
+    WarcriesExertAnAdditionalAttack,
+    WarcryCooldownSpeed,
+    WarcryDamage,
+    WarcryEffect,
+    WarcrySpeed,
+    WardRegenerationRate,
+    WeaponCasterDamagePrefix,
+    WeaponDamageTypePrefix,
+    WeaponSwapSpeed,
+    WitheredEffect,
+    YouCannotBeHindered,
+}
+
+#[derive(Debug)]
+pub enum Affix {
+    Prefix,
+    Suffix,
+    Socket,
+    Corrupted,
+    Unknown(String),
+}
+
+impl From<&str> for Affix {
+    fn from(s: &str) -> Self {
+        match s {
+            "Prefix" => Affix::Prefix,
+            "Suffix" => Affix::Suffix,
+            "Socket" => Affix::Socket,
+            "Corrupted" => Affix::Corrupted,
+            other => Affix::Unknown(other.to_string()),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum ModifierGroup {
+    Base,
+    Desecrated,
+    Essence,
+    Unknown,
+}
+#[derive(Debug)]
+pub struct ModifierId(pub u16);
+#[derive(Debug)]
+pub struct BaseId(pub u16);
+#[derive(Debug)]
+pub struct ItemId(pub u16);
+
+#[derive(Debug)]
+pub struct NewBaseItem {
+    pub id: ItemId,
+    pub base_id: BaseId,
+    pub name: String,
+    pub modifiers: Vec<BaseModifier>,
+}
+
+#[derive(Debug)]
+pub struct BaseModifier {
+    pub id: ModifierId,
+    pub group: ModifierGroup,
+    pub affix: Affix,
+    pub tiers: Vec<ModifierTier>,
+}
+
+#[derive(Debug)]
+pub struct ModifierTier {
+    pub item_level: u8,
+    pub weighting: u16,
+    pub values: Vec<Vec<f32>>,
+}
+
+// --- The Importer Function ---
+
+/// Reads the file once, extracts EVERY base item into memory,
+/// and immediately drops the raw JSON tree.
+pub fn load_all_items(path: &str) -> Result<Vec<NewBaseItem>, Box<dyn std::error::Error>> {
+    let file = File::open(path)?;
+    let root: Value = serde_json::from_reader(file)?;
+
+    let mut extracted_items = Vec::new();
+
+    let bitems = match root.pointer("/bitems/seq").and_then(|v| v.as_array()) {
+        Some(arr) => arr,
+        None => return Ok(extracted_items),
+    };
+
+    // Process every single item in the array
+    for bitem in bitems {
+        let name = bitem["name_bitem"].as_str().unwrap_or("");
+
+        let id = ItemId(parse_u16(&bitem["id_bitem"]).unwrap_or(0));
+        let base_id = BaseId(parse_u16(&bitem["id_base"]).unwrap_or(0));
+        let base_id_str = base_id.0.to_string();
+
+        let mut modifiers = Vec::new();
+        if let Some(mod_id_list) = root
+            .pointer(&format!("/basemods/{}", base_id_str))
+            .and_then(|v| v.as_array())
+        {
+            for mod_val in mod_id_list {
+                if let Some(mod_id_str) = mod_val.as_str() {
+                    if let Some(modifier) = build_modifier(&root, mod_id_str, &base_id_str) {
+                        modifiers.push(modifier);
+                    }
+                }
+            }
+        }
+
+        extracted_items.push(NewBaseItem {
+            id,
+            base_id,
+            name: name.to_string(),
+            modifiers,
+        });
+    }
+
+    Ok(extracted_items)
+}
+
+// --- Internal Helper Functions ---
+
+fn build_modifier(root: &Value, mod_id_str: &str, base_id_str: &str) -> Option<BaseModifier> {
+    let mod_seq = root.pointer("/modifiers/seq")?.as_array()?;
+    let mod_data = mod_seq.iter().find(|m| {
+        parse_u16(&m["id_modifier"]).map(|id| id.to_string()) == Some(mod_id_str.to_string())
+    })?;
+
+    let id = ModifierId(mod_id_str.parse().ok()?);
+    let affix = Affix::from(mod_data["affix"].as_str().unwrap_or(""));
+
+    let group_str = mod_data["modgroups"].as_str().unwrap_or("[]");
+    let group = if group_str.contains("Essence") {
+        ModifierGroup::Essence
+    } else if group_str.contains("Desecrated") {
+        ModifierGroup::Desecrated
+    } else {
+        ModifierGroup::Base
+    };
+
+    let mut tiers = Vec::new();
+    if let Some(tier_list) = root
+        .pointer(&format!("/tiers/{}/{}", mod_id_str, base_id_str))
+        .and_then(|v| v.as_array())
+    {
+        for tier_val in tier_list {
+            let item_level = parse_u16(&tier_val["ilvl"]).unwrap_or(0) as u8;
+            let weighting = parse_u16(&tier_val["weighting"]).unwrap_or(0);
+
+            let mut values = Vec::new();
+            if let Some(nval_str) = tier_val["nvalues"].as_str() {
+                if let Ok(parsed) = serde_json::from_str::<Vec<Vec<f32>>>(nval_str) {
+                    values = parsed;
+                }
+            }
+
+            tiers.push(ModifierTier {
+                item_level,
+                weighting,
+                values,
+            });
+        }
+    }
+
+    Some(BaseModifier {
+        id,
+        group,
+        affix,
+        tiers,
+    })
+}
+
+fn parse_u16(val: &Value) -> Option<u16> {
+    if let Some(n) = val.as_u64() {
+        Some(n as u16)
+    } else if let Some(s) = val.as_str() {
+        s.parse().ok()
+    } else {
+        None
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,157 +1,16 @@
-use std::{collections::HashMap, fs::File};
+use crate::importer::load_all_items;
 
-use serde::{Deserialize, Deserializer};
-use serde_json::json;
+mod importer;
 
-fn de_number_from_string<'de, D, T>(deserializer: D) -> Result<T, D::Error>
-where
-    D: Deserializer<'de>,
-    T: std::str::FromStr + Deserialize<'de>,
-    T::Err: std::fmt::Display,
-{
-    #[derive(Deserialize)]
-    #[serde(untagged)]
-    enum StringOrNumber<T> {
-        String(String),
-        Number(T),
-    }
-
-    match StringOrNumber::<T>::deserialize(deserializer)? {
-        StringOrNumber::String(s) => s.parse().map_err(serde::de::Error::custom),
-        StringOrNumber::Number(n) => Ok(n),
-    }
-}
-
-#[derive(Debug, Deserialize)]
-struct Database {
-    bitems: BItems,
-    modifiers: Modifiers,
-    tiers: HashMap<String, HashMap<String, Vec<Tier>>>,
-    basemods: HashMap<String, Vec<String>>,
-}
-
-#[derive(Debug, Deserialize)]
-struct Tier {
-    #[serde(deserialize_with = "de_number_from_string")]
-    ilvl: u16,
-    #[serde(deserialize_with = "de_number_from_string")]
-    weighting: u16,
-    nvalues: Option<String>,
-}
-
-#[derive(Debug, Deserialize)]
-struct BItems {
-    seq: Vec<BaseItem>,
-}
-
-#[derive(Debug, Deserialize)]
-struct Modifiers {
-    seq: Vec<Modifier>,
-}
-
-#[derive(Debug, Deserialize)]
-struct Modifier {
-    name_modifier: String,
-    #[serde(deserialize_with = "de_number_from_string")]
-    id_modifier: u16,
-    modgroups: String,
-    affix: String,
-}
-
-#[derive(Debug, Deserialize)]
-struct BaseItem {
-    #[serde(deserialize_with = "de_number_from_string")]
-    id_bitem: u16,
-    #[serde(deserialize_with = "de_number_from_string")]
-    id_base: u16,
-
-    name_bitem: String,
-
-    #[serde(deserialize_with = "de_number_from_string")]
-    drop_level: u8,
-}
-
-struct NewBaseItem {
-    id: u16,
-    base_id: u16,
-    possible_modifiers: Vec<NewModifier>,
-}
-
-enum Affix {
-    Prefix,
-    Suffix,
-    Socket,
-    Corrupted,
-}
-
-struct NewModifier {
-    id: u16,
-    group_id: u16,
-    affix: Affix,
-    item_level: u16,
-    weighting: u16,
-    n_values: Option<Vec<Vec<f32>>>,
-}
-
-// #[derive(Debug)]
-// struct CraftedItem {
-//     prefixes: Vec<>
-// }
 
 fn main() {
-    let file = File::open("poec_data.json").unwrap();
+    // Run this once at the start of your program
+    let db_items = load_all_items("poec_data.json").expect("Failed to load and parse database");
 
-    let db: Database = serde_json::from_reader(file).unwrap();
+    println!("Successfully loaded {} items into memory.", db_items.len());
 
-    let vile_robe = db
-        .bitems
-        .seq
-        .iter()
-        .find(|b| b.name_bitem == "Vile Robe")
-        .unwrap();
-
-    let vile_robe_modifiers = db
-        .basemods
-        .get(&vile_robe.id_base.to_string())
-        .unwrap()
-        .iter()
-        .map(|mid| {
-            let modifier = db
-                .modifiers
-                .seq
-                .iter()
-                .find(|m| m.id_modifier.to_string() == *mid)
-                .unwrap();
-
-            let tiers = db
-                .tiers
-                .get(mid)
-                .unwrap()
-                .get(&vile_robe.id_base.to_string())
-                .unwrap();
-
-            let (total_count, max_ilvl, total_weight) = tiers.iter().fold(
-                (0, 0u16, 0u32), // Initial state: (count, max_ilvl, total_weight)
-                |(count, max_ilvl, weight_acc), tier| {
-                    (
-                        count + 1,
-                        max_ilvl.max(tier.ilvl),
-                        weight_acc + tier.weighting as u32, // Cast to u32 to prevent overflow during summation
-                    )
-                },
-            );
-
-            json!({
-                "name": modifier.name_modifier.clone(),
-                "affix": modifier.affix.clone(),
-                "tiers": total_count,
-                "ilvl": max_ilvl,
-                "weigts": total_weight,
-            })
-        })
-        .collect::<Vec<_>>();
-
-    println!("{:#?}", vile_robe);
-
-    println!("{:#?}", vile_robe_modifiers);
+    // Optional: Just print the first one to verify it worked without blowing up your terminal
+    if let Some(first) = db_items.first() {
+        println!("{:#?}", first);
+    }
 }
